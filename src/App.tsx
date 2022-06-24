2780742c042cf54a7107f381f3c1f2333ec4c679
TheusZp <matheuszoppe@hotmail.com>
2022-06-24 20:37:51 -0300

    First commit no fim, pq esqueci msm

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
+}
+
+export default App;
